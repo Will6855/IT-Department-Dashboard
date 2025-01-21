c3f43248fa5c3f8bf25a54cadea2067a7b164cff
Will <86987398+Will6855@users.noreply.github.com>
2025-01-21 16:34:19 +0100

    fix+feat: Update requirements.txt & Added Production server config

--- run.py
+++ run.py
@@ -378,4 +378,10 @@ def internal_server_error(e):
 
 # Launch App Server
 if __name__ == '__main__':
-  app.run(debug=True)
+  # Debug/Development
+  # app.run(debug=True, host="0.0.0.0", port="5000")
+
+  # Production
+  from waitress import serve
+  serve(app, host="0.0.0.0", port=5000)
+  
